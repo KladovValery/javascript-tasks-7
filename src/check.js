7a74a8819bd13d4774de99e4f2dd10527b8a624f
KladovValery <valerijkladov@yandex.ru>
2015-11-17 00:05:10 +0500

    Update check.js

--- src/check.js
+++ src/check.js
@@ -1,5 +1,18 @@
 'use strict';
-
 exports.init = function () {
-
+    Object.prototype.checkContainsKeys = function (keys) {
+        var result = keys.filter(function (property) {
+            return (property in this);
+        });
+        return result.length === keys.length;
+    };
+    Object.prototype.checkHasValueType = function (key, type) {
+        return this[key] == type(this[key]);
+    };    
+    Function.prototype.checkHasParamsCount = function (count) {
+        return this.length === count;
+    };
+    String.prototype.checkHasWordsCount = function (count) {
+        return this.split(' ').length === count;
+    };
 };
